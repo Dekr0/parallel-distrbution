fec20c227da57cc2d1d86b04d318782ffa158533
Dekr0 <dekr0.dk@gmail.com>
2023-01-31 23:48:35 -0700

    Factor out the server setup code

---
+++ thread-server/server.h
@@ -0,0 +1,42 @@
+#ifndef THREAD_SERVER_SERVER_H
+#define THREAD_SERVER_SERVER_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#include "common.h"
+
+
+int setup(char * address, long ip) {
+    struct sockaddr_in sock;
+
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    sock.sin_addr.s_addr = inet_addr(address);
+    sock.sin_port = ip;
+    sock.sin_family = AF_INET;
+
+    if (bind(fd, (struct sockaddr*)&sock, sizeof(sock)) < 0) {
+        printf("Socket creation failed\n");
+
+        exit(1);
+    }
+
+    listen(fd, COM_BUFF_SIZE);
+    printf("Server listening on port %ld\n", ip);
+
+    return fd;
+}
+
+
+void * handle(void *);
+
+
+#endif
